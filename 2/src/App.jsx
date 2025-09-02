ebf325066963168a069b69a0c8950361a591140e
madlock <hassi3914@gmail.com>
2025-09-02 18:33:57 +0500

    clean code

--- 2/src/App.jsx
+++ 2/src/App.jsx
@@ -1,67 +1,64 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 
 import './App.css';
 
 export default function App() {
 
-  const HeaderElement = useRef(null);
-  const HeaderHeading = useRef(null);
-  const HeaderButton1 = useRef(null);
-  const HeaderButton2 = useRef(null);
-  const HeaderButton3 = useRef(null);
   const ContainerElement = useRef(null);
-  const Circle1 = useRef(null);
-  const Circle2 = useRef(null);
+  const [headingButtons, setHeadingButtons] = useState(false);
+  const [isHeaderrem,setIsHeaderrem] = useState(false);
+  const [isCircleIn,setIsCircleIn] = useState(true);
+
 
 
 
   return (
-    <div className="main-container" ref={ContainerElement} onScroll={()=>{
-      if(HeaderElement.current){
-        if(ContainerElement.current.scrollTop>20){
-          HeaderElement.current.classList.add('active');
-          Circle1.current.classList.add('active');
+    <div className="main-container" ref={ContainerElement} onScroll={() => {
+      if (ContainerElement.current) {
+        if (ContainerElement.current.scrollTop > window.innerHeight * 0.07) {
+          setIsHeaderrem(true);
+          setIsCircleIn(false);
+
         }
-        else{
-          HeaderElement.current.classList.remove('active');
-          Circle1.current.classList.remove('active');
+        else {
+          setIsHeaderrem(false);
+          setIsCircleIn(true);
 
         }
       }
-    
-      if(HeaderHeading.current){
-        if(ContainerElement.current.scrollTop>window.innerHeight*0.9){
-          HeaderHeading.current.classList.add('active');
-          HeaderButton1.current.classList.add('active');
-          HeaderButton2.current.classList.add('active');
-          HeaderButton3.current.classList.add('active');
+        if (ContainerElement.current.scrollTop > window.innerHeight * 0.9) {
+
+          setHeadingButtons(true);
+
         }
-        else{
-          HeaderHeading.current.classList.remove('active');
-          HeaderButton1.current.classList.remove('active');
-          HeaderButton2.current.classList.remove('active');
-          HeaderButton3.current.classList.remove('active');
+        else {
+          setHeadingButtons(false);
+
         }
-      }
       
+
     }}>
-      <header ref={HeaderElement}>
-        <h2 ref={HeaderHeading}>Portfolio</h2>
+      <header className={isHeaderrem ? 'active' : ''}>
+        <h2 className={headingButtons ? 'active' : ''}>Portfolio</h2>
         <div className="right-side">
-          <button ref={HeaderButton1}>Home</button>
-          <button ref={HeaderButton2}>Projects</button>
-          <button ref={HeaderButton3}>Contact Me</button>
+          <button className={headingButtons ? 'active' : ''}>Home</button>
+          <button className={headingButtons ? 'active' : ''}>Projects</button>
+          <button className={headingButtons ? 'active' : ''}>Contact Me</button>
         </div>
       </header>
-      
-      
+
+
       <div className="home">
-  
-        <div ref={Circle1} className="circle1">
-          <h1>Hi, I'm Hassaan</h1>
+
+        <div className={ isCircleIn ? "circle1" : "circle1 active"}>
+          <div>
+            <h2>Welcome To My Portfolio</h2>
+            <h1>Website</h1>
+
+          </div>
         </div>
-    
-        <div ref={Circle2} className="circle2"></div>
+
+        <div className={ isCircleIn ? "circle2" : "circle2 active"}></div>
       </div>
       <div className="projects">
         <div className="project"></div>
